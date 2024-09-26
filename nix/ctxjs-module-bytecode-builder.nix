2b992ca63ef2d5128a6377e9b9ce40b65e3bc00e
Jann Müller <--global>
2024-09-26 10:37:23 +0200

    Add patch

--- nix/ctxjs-module-bytecode-builder.nix
+++ nix/ctxjs-module-bytecode-builder.nix
@@ -13,9 +13,16 @@
                 rev = version;
                 hash = "sha256-NGwwBhUMicj+5KiI1z8hleEpgyAL3rG1+cvfBhRtf3k=";
             };
+
+            cargoPatches = [
+                ./0001-Add-Cargo.lock.patch
+            ];
             
             cargoLock = {
                 lockFile = ./typst-ctxjs-cargo.lock;
+                outputHashes = {
+                    "wasm-minimal-protocol-0.1.0" = "sha256-Qj9qrFWrib1i5lswQ9wI0y96/bROXdUmVWhoStbvDo0=";
+                };
             };
             # cargoHash = "sha256-D2r7InJnjeyvpWoYFFjXFxPwmY8wKuWQy9zq9qsOcdg=";
 
